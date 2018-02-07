28ff2d4c5e6bf18cf364b3f1ab7dec645c1e37a8
Arghyadeep Giri <arghyadeep@rechargers.co.in>
2018-02-06 17:03:56 -0800

    Add files via upload

---
+++ general topics/general topics/round of 2/HIT.py
@@ -0,0 +1,62 @@
+import csv
+import pandas as pd
+
+level = int(input("Enter total number of CSV files: "))
+input_arr = []
+
+print("Enter CSV file names in order starting from the first: ")
+for i in range(level):
+    input_file = input("Enter input filename(with .csv extension): ")
+    input_arr.append(input_file)
+
+data = []
+for j in range(level):
+    df = pd.read_csv(input_arr[j])
+    data.append(df)
+
+
+datacomp = pd.read_csv('database1.csv')
+
+utt_id = [i for i in range(1,len(datacomp)+1)]
+HIT_ID = [None]*len(datacomp)
+HITTypeID = [None]*len(datacomp)
+
+counter = 0
+for i in range(len(data[0])):
+    temp1 = data[0]['HITId'][i]
+    temp2 = data[0]['HITTypeId'][i]
+    for j in range(1,5):
+        counter += 1
+        if counter % 5 == 0:
+            counter += 1
+        HIT_ID[counter] = temp1
+        HITTypeID[counter] = temp2
+        
+counter = 0
+for i in range(len(data[1])):
+    temp1 = data[1]['HITId'][i]
+    temp2 = data[1]['HITTypeId'][i]
+
+    for j in range(4):
+        HIT_ID[len(data[0])*5 + counter]= temp1
+        HITTypeID[len(data[0])*5 + counter] = temp2
+        counter += 1
+
+
+for i in range(len(datacomp)):
+    if HIT_ID[i] == None:
+        HIT_ID[i] = 'NA'
+    if HITTypeID [i] == None:
+        HITTypeID[i] = 'NA'
+        
+header = ['Utterance_ID','HITTypeID','HIT_ID']
+columns = []
+columns.append(utt_id)
+columns.append(HITTypeID)
+columns.append(HIT_ID)
+
+with open('HIT.csv','w',newline='') as file:
+    writer = csv.writer(file,delimiter=',')
+    writer.writerows([header])
+    writer.writerows(zip(*columns))
+file.close()
---
+++ general topics/general topics/round of 2/database.py
@@ -0,0 +1,57 @@
+import csv
+import pandas as pd
+
+level = int(input("Enter total number of CSV files: "))
+input_arr = []
+
+print("Enter CSV file names in order starting from the first: ")
+for i in range(level):
+    input_file = input("Enter input filename(with .csv extension): ")
+    input_arr.append(input_file)
+
+data = []
+for j in range(level):
+    df = pd.read_csv(input_arr[j])
+    data.append(df)
+
+texts = []
+dialogueacts = []
+uniqueID = []
+
+for i in range (len(input_arr)):
+    for j in range(len(data[i])):
+        if i == 0:
+            texts.append(data[0]['Input.slot0'][j])
+            dialogueacts.append('prompt')
+            texts.append(data[0]['Answer.TalkMore'][j])
+            dialogueacts.append('TalkMore')
+            texts.append(data[0]['Answer.negative'][j])
+            dialogueacts.append('negative')
+            texts.append(data[0]['Answer.positive'][j])
+            dialogueacts.append('positive')
+            texts.append(data[0]['Answer.rhetorical'][j])
+            dialogueacts.append('rhetorical')
+        else:
+            texts.append(data[i]['Answer.change'][j])
+            dialogueacts.append('change')
+            texts.append(data[i]['Answer.continuation'][j])
+            dialogueacts.append('continuation')
+            texts.append(data[i]['Answer.elaboration'][j])
+            dialogueacts.append('elaboration')
+            texts.append(data[i]['Answer.joking_sarcastic'][j])
+            dialogueacts.append('joking_sarcastic')
+
+for i in range(1,len(texts)+1):
+    uniqueID.append(str((5 - len(str(i)))*'0'+str(i)))
+    
+header = ['UniqueID','DialogueActs','Texts']
+columns = []
+columns.append(uniqueID)
+columns.append(dialogueacts)
+columns.append(texts)
+
+with open('database1.csv','w',newline='') as file:
+    writer = csv.writer(file,delimiter=',')
+    writer.writerows([header])
+    writer.writerows(zip(*columns))
+file.close()
